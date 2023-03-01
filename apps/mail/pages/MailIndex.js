1a5be3e7bfee5e7f83ef6f21c3a97471fc900e4c
Ido <ido.tsehori@gmail.com>
2023-03-01 11:51:47 +0200

    first layout

---
+++ apps/mail/pages/MailIndex.js
@@ -0,0 +1,56 @@
+export default {
+  template: `
+  <!-- <header> -->
+    <section class="main-container">
+        <section class="side-bar-conatiner">
+            <ul>
+                <button>Compose</button>
+                <li>Inbox</li>
+                <li>Starred</li>
+                <li>Snoozed</li>
+                <li>Important</li>
+                <li>Sent</li>
+           </ul>
+        </section>
+        
+        <section class="mails-conatiner">
+            <nav class="nav-bar">
+                <input type="checkbox"/>
+                <!-- Todo  select bar -->
+                <button>Refresh</button>
+                <!-- <button>Mark all</button> -->
+                <button><</button>
+                <button>></button>
+            </nav>
+
+          <section class="mail-list">
+              <ul >
+                  <li>
+                    <div class="1">
+                        <input type="checkbox"/>
+                        <input type="checkbox"/>
+                        <input type="checkbox"/>
+                    </div>
+
+                    <div class="sender">
+                        <h4>dudufaruk</h4>
+                    </div>
+                    
+                    <div class="content">
+                    <h4>kakaka , she she lulu - </h4>
+                    <p> blalalalalaalalalalallgfgfgfgfgfganhdkjdhkjshdsjkh</p>
+                    </div>
+
+                    <div>
+                        <h5>Date</h5>
+                    </div>
+                  </li>
+                </ul>
+            </section>
+
+        </section>
+            
+        </section>
+   </section>
+    `,
+}
--- routes.js
+++ routes.js
@@ -1,20 +1,24 @@
 import HomePage from './views/HomePage.js'
 import AboutUs from './views/AboutUs.js'
-
+import MailIndex from './apps/mail/pages/MailIndex.js'
 const { createRouter, createWebHashHistory } = VueRouter
 
 const routerOptions = {
-	history: createWebHashHistory(),
-	routes: [
-		{
-			path: '/',
-			component: HomePage,
-		},
-		{
-			path: '/about',
-			component: AboutUs,
-		},
-	],
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      component: HomePage,
+    },
+    {
+      path: '/about',
+      component: AboutUs,
+    },
+    {
+      path: '/mail',
+      component: MailIndex,
+    },
+  ],
 }
 
 export const router = createRouter(routerOptions)
